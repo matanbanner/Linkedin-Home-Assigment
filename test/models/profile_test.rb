7a51bf7df03c3ae9c0a4bc20a76daea89ddd1380
Matan Banner <matan.banner@gmail.com>
2017-01-18 19:51:14 +0200

    Test for profile search

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class ProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "find by name" do
+    profile1 = Profile.filter({name: "Bracha Madar"}).first
+    profile2 = Profile.filter({name: "Neta Kaplan Shertzer"}).first
+
+    bracha = profiles(:profile_3)
+
+    assert_equal profile1, bracha, "Didn't match profiles"
+    assert_not_equal profile2, bracha, "Match profiles"
+
+  end
+
+  test "find by name and title" do
+    profile1 = Profile.filter({name: "Bracha Madar", title: "HR Recruiter"}).first
+    bracha = profiles(:profile_3)
+    assert_equal profile1, bracha, "name didn't match"
+    assert_equal profile1, bracha, "Didn't match profiles"
+
+    profile2 = Profile.filter({name: "Bracha Madar", title: "Fake Title"}).first
+    assert_nil profile2, "Should not match any profile"
+  end
+
+
+  test "find by skills" do
+    profile1 = profiles(:profile_7)
+    profile2 = profiles(:profile_8)
+    set1 = [profile1, profile2].to_set
+
+    set2 = Profile.filter({}, ["Linux", "Virtualization", "DevOps"]).to_set
+
+    assert set1 == set2
+
+  end
+
+  test "find by names and skills" do
+    profile1 = Profile.filter({name: "Itai Ganot"}, ["Linux", "Virtualization", "DevOps"]).first
+    profile2 = profiles(:profile_8)
+    assert_equal profile1, profile2, "Didn't match profiles"
+  end
+
+
 end
